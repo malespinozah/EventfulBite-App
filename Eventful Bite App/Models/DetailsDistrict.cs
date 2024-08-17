ae75cbcd1a66d54307f5899bfbd639ed87b4dc48
Maria Alejandra Espinoza Huamani <malespinozah@gmail.com>
2024-08-17 01:42:28 -0400

    commit message

---
+++ Eventful Bite App/Models/DetailsDistrict.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eventful_Bite_App.Models
+{
+    public class DetailsDistrict
+    {
+        public DistrictDto SelectedDistrict { get; set; }
+        public IEnumerable<EventDto> KeptEvents { get; set; }
+        public IEnumerable<BranchDto> Branches { get; set; } = new List<BranchDto>();
+    }
+}
